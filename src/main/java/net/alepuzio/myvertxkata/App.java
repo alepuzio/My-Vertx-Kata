2d91a86bc88e6bf7e71d8d14eeaa8ccd50ac0b3f
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-19 12:22:11 +0200

    refactoring

--- src/main/java/net/alepuzio/myvertxkata/App.java
+++ src/main/java/net/alepuzio/myvertxkata/App.java
@@ -1,22 +1,18 @@
 package net.alepuzio.myvertxkata;
 
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Route;
-import io.vertx.ext.web.Router;
-import net.alepuzio.myvertxkata.basic.GetCommand;
-import net.alepuzio.myvertxkata.basic.PostCommand;
+import net.alepuzio.myvertxkata.basic.BasicScenario;
+
 
 public class App {
 
 	public static void main(String[] args) {
-		Vertx vertx = Vertx.vertx();
-		HttpServer httpServer = vertx.createHttpServer();
-		Router router = Router.router(vertx);
-		Route handler1 = new GetCommand(router, "/hello/:name").route();
-		Route handler2 = new PostCommand(router, "/hello/").route();
-		httpServer.requestHandler(router::accept).listen(8091);
-
+	
+		App app = new App();
+		app.basicScenario();
 	}
 
+	
+	public void basicScenario() {
+		new BasicScenario(8091).execute();
+	}
 }
