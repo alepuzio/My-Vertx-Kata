d07d381dca36a68b555fd412d8c83ab33bd506c6
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-20 15:38:46 +0200

    copied from marcorotondi github, use of the route

---
+++ src/main/java/net/alepuzio/myvertxkata/route/model/PostBean.java
@@ -0,0 +1,41 @@
+package net.alepuzio.myvertxkata.route.model;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PostBean {
+
+    private static final AtomicInteger COUNTER = new AtomicInteger();
+    private final int id;
+    private String title;
+    private String message;
+
+    public PostBean() {
+        this.id = COUNTER.getAndIncrement();
+    }
+
+    public PostBean(String title, String message) {
+        this.id = COUNTER.getAndIncrement();;
+        this.title = title;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
