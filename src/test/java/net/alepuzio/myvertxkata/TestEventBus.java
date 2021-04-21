810937d144c5fa5571e7adb599d761f661dbaf69
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-21 12:17:36 +0200

    verify One Vertx = One EventBus

---
+++ src/test/java/net/alepuzio/myvertxkata/TestEventBus.java
@@ -0,0 +1,30 @@
+package net.alepuzio.myvertxkata;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+
+
+public class TestEventBus  {
+
+	@Test
+	public void testEventBusEqualsInSamevertx() {
+		final Vertx vertx = Vertx.vertx();// entry point
+		final EventBus eventBus1 = vertx.eventBus();
+		final EventBus eventBus2 = vertx.eventBus();
+		/*
+		 * One <i>Vertx</i> points to the one <i>EventBus</i>
+		 * */
+		Assertions.assertEquals(eventBus1, eventBus2);
+	}
+
+	@Test
+	public void testEventBusEqualsInDifferentVertx() {
+		final EventBus eventBus1 = Vertx.vertx().eventBus();
+		final EventBus eventBus2 = Vertx.vertx().eventBus();
+		Assertions.assertNotEquals(eventBus1, eventBus2);
+	}
+
+}
